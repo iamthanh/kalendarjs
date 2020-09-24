1feba06874dea9464b560adf9518797615bc85e6
unknown <nick120187@gmail.com>
2020-09-24 14:23:25 -0700

    created schema/model for event; finished fetching all events

--- server/server.ts
+++ server/server.ts
@@ -1,13 +1,11 @@
 const express = require("express");
 const mongoose = require('mongoose');
+import Events from './models/eventModel';
 
 var env = process.env.NODE_ENV || 'development';
 var config = require('../config/config')[env];
 
-console.log(config);
-
 const port = config.server.port;
-
 const app = express();
 
 mongoose.connect(config.database.mongodb.url, config.database.mongodb.options);
@@ -18,23 +16,35 @@ db.once('open', function() {
 });
 
 // Getting all events
-app.get('/api/events', (req, res) => {
-  console.log('requesting to get all events');
-
-  
-
+app.get('/api/events', async (req:any, res:any) => {
+  let events = await Events.find();
+  res.send(events);
 })
 
-// Getting an individual event
-app.get('/api/event/:eventId', (req, res) => {
+// app.get('/api/event/test', async (req:any, res:any) => {
+//   let newEvent = new Events();
 
+//   newEvent.owner = 123;
+//   newEvent.created = new Date();
+//   newEvent.title = 'hello testing'
+
+//   await newEvent.save();
+//   res.send(newEvent)
+// })
+
+// Getting an individual event
+app.get('/api/event/:eventId', async (req:any, res:any) => {
+  let event = await Events.findById(req.params.eventId);
+  res.send(event);
 })
 
 // Creates a new event
-app.post('/api/event', (req, res) => {
+app.post('/api/event', (req:any, res:any) => {
 
 })
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
+
+export {};
