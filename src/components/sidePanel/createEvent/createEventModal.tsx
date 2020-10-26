626cffdb174ac0b5e058acc7b4af2dad73a87a38
unknown <nick120187@gmail.com>
2020-10-26 15:48:10 -0700

    some code cleanup; going with easist solution for now

--- src/components/sidePanel/createEvent/createEventModal.tsx
+++ src/components/sidePanel/createEvent/createEventModal.tsx
@@ -15,34 +15,40 @@ type createEventModal = {
 
 function CreateEventModal(props: createEventModal) {
 
-  const now = new Date('10/6/2020 10:12AM');
-  const [startDateTime, setStartDateTime] = useState<Date>(now);
-  const [endDateTime, setEndDateTime] = useState<Date>(now);
+  // const nowStart = new Date();
+  // now.setMinutes(1);
 
-  const [startTime, setStartTime] = useState<string>('');
-  const [endTime, setEndTime] = useState<string>('');
+
+
+
+  // console.log('now: ' + now);
+
+  const [startDateTime, setStartDateTime] = useState<Date>(new Date());
+  const [endDateTime, setEndDateTime] = useState<Date>(new Date());
 
   const [allDay, setAllDay] = useState<boolean>(false);
 
   useEffect(() => {
 
-    if (now.getMinutes() < 15) {
-      setStartDateTime(time => new Date(time.setMinutes(15)))
-      setEndDateTime(time => new Date(time.setMinutes(30)))
-    } else if (now.getMinutes() < 30) {
-      setStartDateTime(time => new Date(time.setMinutes(30)))
-      setEndDateTime(time => new Date(time.setMinutes(45)))
-    } else if (now.getMinutes() < 45) {
-      setStartDateTime(time => new Date(time.setMinutes(45)))
-      setEndDateTime(time => {
-        time.setMinutes(0);
-        time.setHours(time.getHours() + 1);
-        return new Date(time)
-      })
-    } else {
-      setStartDateTime(time => new Date(time.setMinutes(60)))
-      setEndDateTime(time => new Date(time.setMinutes(15)))
-    }
+    // setStartDateTime(time => new Date(time.setMinutes(30)))
+
+    // if (startDateTime.getMinutes() < 15) {
+    //   setStartDateTime(time => new Date(time.setMinutes(15)))
+    //   setEndDateTime(time => new Date(time.setMinutes(30)))
+    // } else if (startDateTime.getMinutes() < 30) {
+    //   setStartDateTime(time => new Date(time.setMinutes(30)))
+    //   setEndDateTime(time => new Date(time.setMinutes(45)))
+    // } else if (startDateTime.getMinutes() < 45) {
+    //   setStartDateTime(time => new Date(time.setMinutes(45)))
+    //   setEndDateTime(time => {
+    //     time.setMinutes(startDateTime.getMinutes()+15);
+    //     // time.setHours(time.getHours());
+    //     return new Date(time)
+    //   })
+    // } else {
+    //   setStartDateTime(time => new Date(time.setMinutes(60)))
+    //   setEndDateTime(time => new Date(time.setMinutes(15)))
+    // }
 
     // Setting the inital start and end times
     // setInitalTimes();
@@ -50,33 +56,16 @@ function CreateEventModal(props: createEventModal) {
 
   const setInitalTimes = () => {
 
-    console.log(now);
+    // console.log(now);
 
-    setStartTime(now.getHours() + ':' + now.getMinutes());
+    // setStartTime(now.getHours() + ':' + now.getMinutes());
 
-    console.log(now.getMinutes());
+    // console.log(now.getMinutes());
     // setEndTime(now.getHours())
   }
 
   const cancelHandler = () => {
     props.handleClose();
-  }
-
-  useEffect(() => {
-    console.log(endDateTime);
-  }, [endDateTime]);
-
-  let timeOptions: Array<JSX.Element> = [];
-  for (let hour = 0; hour < 24; hour++) {
-    for (let min = 0; min < 60; min += 15) {
-      let suffix = hour >= 12 ? 'PM' : 'AM';
-      let _hour = hour >= 13 ? hour - 12 : hour;
-      let _min = min === 0 ? '00' : min;
-      let selected = _hour === startDateTime.getHours() && min === startDateTime.getMinutes() ? true : false;
-      timeOptions.push(
-        <option selected={selected} value={hour + ':' + min}>{_hour + ':' + _min + suffix}</option>
-      )
-    }
   }
 
   const setTimeHandler = (setter, time: string) => {
@@ -88,57 +77,65 @@ function CreateEventModal(props: createEventModal) {
       time.setHours(parseInt(_time[0]));
       return new Date(time)
     })
-
-    // Update the end time 
-
-
   }
 
-  const endTimeHandler = (time: string) => {
-    let _time = time.split(':');
-    setEndDateTime(time => {
-      time.setMinutes(parseInt(_time[1]));
-      time.setHours(parseInt(_time[0]));
+  // const endTimeHandler = (time: string) => {
+  //   let _time = time.split(':');
+  //   setEndDateTime(time => {
+  //     time.setMinutes(parseInt(_time[1]));
+  //     time.setHours(parseInt(_time[0]));
 
-      // console.log?
-      return new Date(time)
+  //     // console.log?
+  //     return new Date(time)
+  //   })
+  // }
+
+  const submitHandler = () => {
+    console.log({
+      'startDateTime': startDateTime,
+      'endDateTime':endDateTime,
+      'allDay': allDay
     })
   }
 
   return (
-    <React.Fragment>
-      <Modal
-        show={props.show}
-        onHide={cancelHandler}
-        backdrop="static"
-        keyboard={false}
-      >
-        <Modal.Header closeButton={false}>
-          <Modal.Title>Create event</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
+    <Modal
+      show={props.show}
+      onHide={cancelHandler}
+      backdrop="static"
+      keyboard={false}
+    >
+      <Modal.Header closeButton={false}>
+        <Modal.Title>Create event</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
 
-          <Form>
-            <Form.Group controlId="__title">
-              <Form.Control size="sm" type="text" placeholder="Enter the title" />
+        <Form>
+          <Form.Group controlId="__title">
+            <Form.Control size="sm" type="text" placeholder="Enter the title" />
+          </Form.Group>
+
+          <Form.Group controlId="__description">
+            <Form.Control as="textarea" rows={4} placeholder="Description" />
+          </Form.Group>
+
+          <Form.Row>
+            <Form.Group as={Col} controlId="__startDateTime">
+              <Form.Label>Start</Form.Label>
+              <DatePicker selected={startDateTime} onChange={date => setStartDateTime(date)} />
             </Form.Group>
 
-            <Form.Group controlId="__description">
-              <Form.Control as="textarea" rows={4} placeholder="Description" />
+            <Form.Group as={Col} controlId="__endDateTime">
+              <Form.Label>End</Form.Label>
+              <DatePicker selected={endDateTime} onChange={date => setEndDateTime(date)} />
             </Form.Group>
+          </Form.Row>
 
-            <Form.Row>
-              <Form.Group as={Col} controlId="__startDateTime">
-                <Form.Label>Start</Form.Label>
-                <DatePicker selected={startDateTime} onChange={date => setStartDateTime(date)} />
-              </Form.Group>
+          <Form.Group controlId="__allDay">
+            <Form.Check type="checkbox" label="All day" defaultChecked={allDay} onChange={e => setAllDay(e.target.checked)} />
+          </Form.Group>
 
-              <Form.Group as={Col} controlId="__endDateTime">
-                <Form.Label>End</Form.Label>
-                <DatePicker selected={endDateTime} onChange={date => setEndDateTime(date)} />
-              </Form.Group>
-            </Form.Row>
-
+          {!allDay && (
             <Form.Row>
               <Form.Group as={Col} controlId="__startDateTime">
                 <Form.Label>Start time</Form.Label>
@@ -162,14 +159,15 @@ function CreateEventModal(props: createEventModal) {
                 />
               </Form.Group>
             </Form.Row>
-          </Form>
-        </Modal.Body>
-        <Modal.Footer>
-          <Button variant="secondary" onClick={() => cancelHandler()}>Cancel</Button>
-          <Button variant="primary">Create</Button>
-        </Modal.Footer>
-      </Modal>
-    </React.Fragment>
+          )}
+
+        </Form>
+      </Modal.Body>
+      <Modal.Footer>
+        <Button variant="secondary" onClick={() => cancelHandler()}>Cancel</Button>
+        <Button variant="primary" onClick={submitHandler}>Create</Button>
+      </Modal.Footer>
+    </Modal>
   );
 }
 
